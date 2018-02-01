5184f3eb57d6e7f2f9551cf0ddb88ef47c3f3524
Brittany Bellantoni <brittt2121@hotmail.com>
2018-02-01 20:25:12 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,7 @@
+def valid_move? (board, index)
+  if  board[index] == nil || board[index] == 'X' || board[index] == 'O' || index.between?(0, 8) == false
+    false
+  elsif board[index] == ' ' || board[index] == ''
+    true
+  end
+end
